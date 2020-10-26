33b2ef9d37b6eeefe734fb9d8e9b6e49b67589b2
Anton Sviridov <keynmol@gmail.com>
2020-10-26 19:50:50 +0000

    Fix Linker on 2.12

--- core/src/main/scala/Linker.scala
+++ core/src/main/scala/Linker.scala
@@ -1,11 +1,11 @@
 package com.indoorvivants.subatomic
 
 class Linker(content: Vector[(SitePath, _)], base: SitePath) {
-  private val mp = content.toMap;
+  private val mp = content.map(_._1).toSet;
 
   def rooted(f: (SiteRoot with SitePath => SitePath)): String = {
     val rawLocation = f(SiteRoot)
-    if (mp.contains(rawLocation)) rawLocation.prepend(base).toString
+    if (mp(rawLocation)) rawLocation.prepend(base).toString
     else
       throw new IllegalArgumentException(
         s"Could not resolve $rawLocation location in the site"
